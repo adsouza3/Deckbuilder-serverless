eab33c2b738c17d64ae6c6ca2020b48dd65a8e5d
Anthony <anthony@buildout.com>
2025-04-02 21:13:40 -0500

    Back to old card image logic

--- src/lib/image_urls.js
+++ src/lib/image_urls.js
@@ -1,20 +1,20 @@
-import md5 from 'md5';
+// import md5 from 'md5';
 
-const urls = {};
+// const urls = {};
 export const getImageUrl = (card) => {
-  // TODO uncomment if Eternal Warcry becomes usable again for images
-  // return card.imageUrl
+  return card.imageUrl;
 
-  if (urls[card.name]) {
-    return urls[card.name];
-  }
+  // TODO uncomment if EternalWarcry becomes unusable again for images
+  // if (urls[card.name]) {
+  //   return urls[card.name];
+  // }
 
-  const sanitizedFilename = `${card.name.replace(/\s/g, '_')}.png`;
-  const hash = md5(sanitizedFilename);
-  const urlPrefix = 'https://static.wikia.nocookie.net/eternalcardgame/images';
-  const url = `${urlPrefix}/${hash.charAt(0)}/${hash.charAt(0)}${hash.charAt(1)}/${sanitizedFilename}`;
+  // const sanitizedFilename = `${card.name.replace(/\s/g, '_')}.png`;
+  // const hash = md5(sanitizedFilename);
+  // const urlPrefix = 'https://static.wikia.nocookie.net/eternalcardgame/images';
+  // const url = `${urlPrefix}/${hash.charAt(0)}/${hash.charAt(0)}${hash.charAt(1)}/${sanitizedFilename}`;
 
-  urls[card.name] = url;
+  // urls[card.name] = url;
 
-  return url;
+  // return url;
 };
